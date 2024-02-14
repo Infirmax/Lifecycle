b37e8ded0a5d1576eacce9d71f2d46efd0b221c0
Kevin Wu <77449439+Infirmax@users.noreply.github.com>
2024-02-14 16:07:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.lifecycle
+
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lifecycle.ui.theme.LifecycleTheme
+
+
+
+
+private const val TAG = "MainActivity"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        setContentView(R.layout.activity_main)
+        val hello = getString(R.string.hello)
+
+        val buttona = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val editText = findViewById<EditText>(R.id.editTextText)
+
+
+        buttona.setOnClickListener {
+            textView.text = hello
+            editText.text = Editable.Factory.getInstance().newEditable(hello)
+        }
+
+//        binding = MainActivityBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+}
+
+
+
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        LifecycleTheme {
+            Greeting("Android")
+        }
+    }
